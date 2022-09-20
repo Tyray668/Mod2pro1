d8368dcbcac1af7fd18d0e069714c8b17b82a0e8
Tyray668 <113401005+Tyray668@users.noreply.github.com>
2022-09-19 20:36:42 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab1Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool cond = true;
+
+
+
+            // Logic Block
+
+            int x = 0;
+
+            do
+            {
+                Console.WriteLine(x * 2);
+                Console.WriteLine(x + 6);
+                Console.WriteLine(x + 7);
+                Console.WriteLine(x + 9);
+                Console.WriteLine(x + 10);
+                x++;
+                if (x > 8)
+                {
+                    cond = false;
+                }
+            } while (cond);
+
+            cond = false;
+
+            while (cond) ;
+
+            Console.WriteLine("***************************************************");
+            Console.WriteLine("*****************************************************************************");
+            Console.WriteLine(x * 11);
+            Console.WriteLine(x + 1);
+            Console.WriteLine(x * 12);
+            Console.WriteLine(x * 5);
+            Console.WriteLine(x * 6);
+
+            cond = true;
+
+            while (cond)
+            {
+                Console.WriteLine(x / 8);
+                Console.WriteLine(x / 8);
+                Console.WriteLine(x / 9);
+                Console.WriteLine(x / 10);
+                Console.WriteLine(x / 7);
+
+                x++;
+                if (x > 8)
+                {
+                    cond = false;
+
+                }
+                cond = true;
+
+                while (cond) ;
+                {
+                    Console.WriteLine(x - 1);
+                    Console.WriteLine(x - 4);
+                    Console.WriteLine(x - 5);
+                    Console.WriteLine(x - 6);
+                    Console.WriteLine(x - 8);
+
+                }
+            }
+            Console.Read();
+        }
+    }
+}
+
+        
+    
+
+            
+     
+
+      
+                    
+            
+            
+
+
+   
+
+            
+            
+
+        
+         
+        
+
+        
+            
+
